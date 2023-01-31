1d49ee7f03da2e587ef044cace5bc51d58e5a81c
favjiw <mfavianj@gmail.com>
2023-01-31 08:18:17 +0700

    change login style?

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -119,8 +119,16 @@
                                         <td><div class="td-content"><span class="discount-pricing">Sakit</span></div></td>
                                         <td>
                                             <div class="row">
-                                                <button class="btn btn-success mr-3">Setuju</button> 
-                                                <button class="btn btn-danger">Tolak</button> 
+                                                <button class="btn btn-success mr-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                                                      </svg>
+                                                </button> 
+                                                <button class="btn btn-danger">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                                      </svg>    
+                                                </button> 
                                             </div>
                                         </td>
                                     </tr>
@@ -130,8 +138,16 @@
                                         <td><div class="td-content"><span class="discount-pricing">Sakit</span></div></td>
                                         <td>
                                             <div class="row">
-                                                <button class="btn btn-success mr-3">Setuju</button> 
-                                                <button class="btn btn-danger">Tolak</button> 
+                                                <button class="btn btn-success mr-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                                                      </svg>
+                                                </button> 
+                                                <button class="btn btn-danger">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                                      </svg>    
+                                                </button> 
                                             </div>
                                         </td>
                                     </tr>
@@ -141,8 +157,16 @@
                                         <td><div class="td-content"><span class="discount-pricing">Sakit</span></div></td>
                                         <td>
                                             <div class="row">
-                                                <button class="btn btn-success mr-3">Setuju</button> 
-                                                <button class="btn btn-danger">Tolak</button> 
+                                                <button class="btn btn-success mr-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                                                      </svg>
+                                                </button> 
+                                                <button class="btn btn-danger">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                                      </svg>    
+                                                </button> 
                                             </div>
                                         </td>
                                     </tr>
@@ -152,8 +176,16 @@
                                         <td><div class="td-content"><span class="discount-pricing">Sakit</span></div></td>
                                         <td>
                                             <div class="row">
-                                                <button class="btn btn-success mr-3">Setuju</button> 
-                                                <button class="btn btn-danger">Tolak</button> 
+                                                <button class="btn btn-success mr-3">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
+                                                        <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425a.247.247 0 0 1 .02-.022Z"/>
+                                                      </svg>
+                                                </button> 
+                                                <button class="btn btn-danger">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                                                      </svg>    
+                                                </button> 
                                             </div>
                                         </td>
                                     </tr>
